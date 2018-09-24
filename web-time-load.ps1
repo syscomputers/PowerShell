cbfa92d33196eb672fb8b82fe826d544b8bf1826
syscomputers <syscomputers@gmail.com>
2018-09-25 00:06:15 +0200

    Add files via upload

---
+++ web-time-load.ps1
@@ -0,0 +1,20 @@
+﻿###############################################################################
+#######               Script https://www.madridinformatico.es           #######
+#######                          versión 1.0                            #######
+#######                           13.09.2018                            #######
+#######                  email:info@madridinformatico.es                #######
+###############################################################################
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$Uri = Read-Host -Prompt 'Introduce la dirección web'
+
+$timeTaken = Measure-Command -Expression {
+$site = Invoke-WebRequest -uri $URI 
+}
+
+$millisegundos = $timeTaken.TotalMilliseconds
+
+$segundos = [Math]::Round($millisegundos/1000, 1)
+
+"La carga de la página se ha realizado en  $segundos segundos"
+
